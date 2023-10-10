5a3f989f7489895cde58b2b6200a31b8c875e62d
surferwat <nextglobalx@protonmail.com>
2023-10-10 13:55:10 +0900

    refactor to handle tokujos with immediate payment collection timings

--- app/controllers/tokujo_sale_orders/succeeded_controller.rb
+++ app/controllers/tokujo_sale_orders/succeeded_controller.rb
@@ -1,38 +1,46 @@
 class TokujoSaleOrders::SucceededController < ApplicationController
+  include CheckoutSessionExistable
   layout "public_for_patrons"
   
   def index
-    authorize :tokujo_sale, :index?
-
-    # Check whether checkout session exists
-    checkout_session = CheckoutSession.find_by(id: params[:checkout_session_id])
-    if checkout_session == nil
-      redirect_to tokujo_sale_path(params[:tokujo_id])
-      return
-    end
+    authorize :tokujo_sale_order, :index?
 
     tokujo = Tokujo.find(params[:tokujo_id])
     order = Order.find(params[:id])
+    checkout_session = @checkout_session
 
-    # A payment method has been successfully attached to the setup intent object 
-    # stored in Stripe's database, so we need to update the item_status for 
-    # this order.
-    order.item_status = :payment_due
-    order.save
+    case tokujo.payment_collection_timing
+    when "immediate"
+      # Payment was collected, so we need to update the item_status and 
+      # status for this order.
+      order.item_status = :payment_received
+      order.status = :closed
+      order.save
 
-    # Update the number of Tokujo items taken.
-    tokujo.number_of_items_taken += order.size
-    tokujo.save 
+      # Close tokujo
+      tokujo.status = :closed
+      tokujo.save
+    when "delayed"
+      # A payment method has been successfully attached to the setup intent object 
+      # stored in Stripe's database, so we need to update the item_status for 
+      # this order.
+      order.item_status = :payment_due
+      order.save
+
+      # Update the number of Tokujo items taken.
+      tokujo.number_of_items_taken += order.size
+      tokujo.save 
+
+      # If the Tokujo status == "closed" and number_of_items_taken == number_of_items_available, 
+      # then we need to queue a job to charge all of the patrons that placed an order for 
+      # this Tokujo.
+      if tokujo.status == :closed and tokujo.number_of_items_taken == tokujo.number_of_items_available
+        ChargePatronPaymentMethodsJob.perform_later tokujo.orders
+      end
+    end
 
     # The checkout session is finished as soon as the patron has reached this page, 
     # so we need to destroy the checkout session instance in our database.
-    checkout_session.destroy
-
-    # If the Tokujo status == "closed" and number_of_items_taken == number_of_items_available, 
-    # then we need to queue a job to charge all of the patrons that placed an order for 
-    # this Tokujo.
-    if tokujo.status == :closed and tokujo.number_of_items_taken == tokujo.number_of_items_available
-      ChargePatronPaymentMethodsJob.perform_later tokujo.orders
-    end
+    checkout_session.destroy 
   end
 end
--- test/controllers/tokujo_sale_orders/succeeded_controller_test.rb
+++ test/controllers/tokujo_sale_orders/succeeded_controller_test.rb
@@ -17,4 +17,38 @@ class TokujoSaleOrders::SucceededControllerTest < ActionDispatch::IntegrationTes
     get tokujo_sale_orders_succeeded_path(@order.id, tokujo_id: @tokujo.id, patron_id: @user_patron.id, checkout_session_id: "")
     assert_redirected_to tokujo_sale_path(tokujo_id: @tokujo.id)
   end
+
+  test "should update order item_status and status and tokujo status when payment collection timing value is immediate" do
+    tokujo_immediate = tokujos(:tokujo_two)
+
+    get tokujo_sale_orders_succeeded_path(@order.id, tokujo_id: tokujo_immediate.id, patron_id: @user_patron.id, checkout_session_id: @checkout_session.id)
+
+    @order.reload
+    tokujo_immediate.reload
+
+    assert_equal @order.item_status, "payment_received"
+    assert_equal @order.status, "closed"
+    assert_equal tokujo_immediate.status, "closed"
+  end
+
+  test "should update order item_status and status and tokujo number_of_items_taken when payment collection timing value is delayed" do
+    tokujo_delayed = tokujos(:tokujo_one)
+
+    tokujo_number_of_items_taken = tokujo_delayed.number_of_items_taken
+
+    get tokujo_sale_orders_succeeded_path(@order.id, tokujo_id: tokujo_delayed.id, patron_id: @user_patron.id, checkout_session_id: @checkout_session.id)
+
+    @order.reload
+    tokujo_delayed.reload
+
+    assert_equal @order.item_status, "payment_due"
+    assert_equal tokujo_delayed.number_of_items_taken, tokujo_number_of_items_taken + @order.size
+  end
+
+  test "should destroy checkout session" do
+    get tokujo_sale_orders_succeeded_path(@order.id, tokujo_id: @tokujo.id, patron_id: @user_patron.id, checkout_session_id: @checkout_session.id)
+    
+    assert_not CheckoutSession.exists?(@checkout_session.id)
+    assert_response :success
+  end
 end
