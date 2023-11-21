45196454fb2fe0122613926f7e144731d6064eb3
surferwat <nextglobalx@protonmail.com>
2023-11-21 15:00:13 +0900

    close #38

--- app/controllers/tokujos/directory_controller.rb
+++
@@ -1,9 +0,0 @@
-class Tokujos::DirectoryController < ApplicationController
-  def index
-    authorize :tokujo, :index?
-
-    # Set instance variables for view
-    @keys = Tokujo.column_names
-    @tokujos = Tokujo.where(user_id: Current.user.id)
-  end
-end
--- app/helpers/tokujos/directory_helper.rb
+++
@@ -1,2 +0,0 @@
-module Tokujos::DirectoryHelper
-end
