39d47acb7f0443d87073c213f804c20519a87c9c
surferwat <nextglobalx@protonmail.com>
2023-12-04 13:59:30 +0900

    close #50

--- test/jobs/soft_delete_checkout_session_job_test.rb
+++ test/jobs/soft_delete_checkout_session_job_test.rb
@@ -1,7 +1,12 @@
 require "test_helper"
 
 class SoftDeleteCheckoutSessionJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should assert job is performed" do
+    checkout_session = checkout_sessions(:checkout_session_three)
+    assert_performed_jobs 0
+    perform_enqueued_jobs do
+      SoftDeleteCheckoutSessionJob.perform_later(checkout_session)
+    end
+    assert_performed_jobs 1
+  end
 end
